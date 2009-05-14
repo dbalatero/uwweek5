2a0a486b1008518f5c39911426210065575ff477
David Balatero <dbalatero@evri.com>
2009-05-13 20:23:32 -0700

    Added raw http generation

--- http_server/lib/http_server/response.rb
+++ http_server/lib/http_server/response.rb
@@ -6,5 +6,34 @@ module HttpServer
       @code = 200
       @headers = {}
     end
+
+    def raw_http
+      resp = ""
+      case @code
+      when 200
+        resp << "HTTP/1.1 200 OK" << HttpServer::CRLF
+        resp << "Date: #{http_date}" << HttpServer::CRLF
+        resp << "Content-Length: #{@body.length}" << HttpServer::CRLF
+        resp << "Content-Type: text/html" << HttpServer::CRLF
+        resp << HttpServer::CRLF
+        resp << @body
+      when 404
+        resp << "HTTP/1.1 404 Not Found" << HttpServer::CRLF
+        resp << HttpServer::CRLF
+        resp << "404 page not found"
+      when 500
+        resp << "HTTP/1.1 500 Internal Server Error" << HttpServer::CRLF
+        resp << HttpServer::CRLF
+        resp << @body
+      else
+        raise "Unsupported HTTP code!"
+      end
+      resp
+    end
+
+    private
+    def http_date
+      Time.now.strftime("%a, %d %b %Y %H:%M:%S %Z")
+    end
   end
 end
--- http_server/spec/http_server/response_spec.rb
+++ http_server/spec/http_server/response_spec.rb
@@ -38,4 +38,50 @@ describe HttpServer::Response do
       @resp.headers['test'].should == 'ok'
     end
   end
+
+  describe "raw_http" do
+    describe "200 responses" do
+      before(:each) do
+        @resp = HttpServer::Response.new
+        @resp.code = 200
+        @resp.body = "bodybody"
+      end
+
+      it "should return 200 OK" do
+        @resp.raw_http.should =~ /HTTP\/1.1 200 OK/
+      end
+
+      it "should return Date, Content-Length, Content-Type" do
+        ["Date:", "Content-Length:", "Content-Type:"].each do |key|
+          @resp.raw_http.should =~ /#{key}/
+        end
+      end
+
+      it "should have a body" do
+        @resp.raw_http.should =~ /bodybody$/
+      end
+    end
+    
+    describe "404 responses" do
+      before(:each) do
+        @resp = HttpServer::Response.new
+        @resp.code = 404
+      end
+
+      it "should return 404 Not Found" do
+        @resp.raw_http.should =~ /HTTP\/1.1 404 Not Found/
+      end
+    end
+
+    describe "500 responses" do
+      before(:each) do
+        @resp = HttpServer::Response.new
+        @resp.code = 500
+      end
+
+      it "should return 500 Internal Server Error" do
+        @resp.raw_http.should =~ /HTTP\/1.1 500 Internal Server Error/
+      end
+    end
+  end
 end
