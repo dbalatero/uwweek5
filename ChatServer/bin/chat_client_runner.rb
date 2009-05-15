5d263e4038834dfab853fe09c0216641e0bb0812
Peter Held <pheld@phubuntu.(none)>
2009-05-15 09:30:54 -0700

    Skeletons for chat server runner and chat client runner.

--- ChatServer/bin/chat_client_runner.rb
+++ ChatServer/bin/chat_client_runner.rb
@@ -1,4 +1,18 @@
 require 'rubygems'
 require 'lib/chat_client'
+require 'drb/drb'
 
+# start the service
+DRb.start_service
 
+# connect to the server
+chat_server = DRbObject.new_with_uri('druby://:31337')
+
+# initialize client
+chat_client = ChatClient.new('Nickname_1', 'User_1', chat_server)
+
+# join a channel
+chat_client.join('Channel_1')
+
+# send a message
+chat_client.send_message('Channel_1', 'This is a TEST message!')
--- ChatServer/bin/chat_server_runner.rb
+++ ChatServer/bin/chat_server_runner.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
+require 'drb/drb'
 require 'lib/chat_server'
 
-DRb.start_service('druby://localhost:31337', ChatServer.new)
+DRb.start_service('druby://:31337', ChatServer.new)
 DRb.thread.join
