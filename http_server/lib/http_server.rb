d37605654e8e7e5ff05db10b96babb4717d9ac0c
David Balatero <dbalatero@evri.com>
2009-05-15 17:12:06 -0700

    added thread pool server

--- http_server/lib/http_server.rb
+++ http_server/lib/http_server.rb
@@ -1,8 +1,10 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
+require 'http_server/server_methods'
 require 'http_server/request'
 require 'http_server/response'
 require 'http_server/router'
+require 'http_server/thread_pool'
 
 module HttpServer
   CRLF = "\r\n"
